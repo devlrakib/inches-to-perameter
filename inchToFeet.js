323ea91bb7fe9e54799adf100ac27c4ebdcd942a
devlrakib <devrakib5@gmail.com>
2024-03-16 19:19:28 +0600

    first commit

---
+++ inchToFeet.js
@@ -0,0 +1,12 @@
+const myInches = 12;
+const myFeet   = myInches/12;
+console.log(myFeet );
+
+
+function inchToFeet (inches){
+    const feet =  inches/12;
+    return feet;
+}
+const dadainches = 144;
+const dadafeet   = inchToFeet (dadainches);
+console.log(dadafeet);
